9ffb710c5f57bebcfc5f0cbec098579fbecec1d1
kakakikikeke <cdl.yoshinaka@gmail.com>
2018-09-21 13:13:11 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+compact-toolbar
+===============
+You can toggle as hide/show bookmark's titles on bookmark toolbar.
+
+How to install
+--------------
+Download addon from below URL.  
+https://addons.mozilla.org/ja/firefox/addon/compact-toolbar/
+
+How to use it
+-------------
+
+![compact-toolbar-demo](https://1.bp.blogspot.com/-eMPvL75MqNM/W6RuQyJIdzI/AAAAAAAAdVg/FSvOO9r_YX4VUoxk4Nub7l8ptMEH1Tw3gCLcBGAs/s1600/compact_toolbar_demo.gif)
+
+1. Click icon therefore toggle hide/show titles on bookmar toolbar.
+
+Bug Report
+-----------
+
+Please sending an issue or pull-request on this repository.
---
+++ main.js
@@ -0,0 +1,116 @@
+var id = "toolbar_____";
+
+function isEmpty(obj) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+    return JSON.stringify(obj) === JSON.stringify({});
+}
+
+function onError(error) {
+  browser.browserAction.enable();
+  console.log(error)
+}
+
+function changeIcon(mode) {
+  browser.browserAction.setIcon({
+    path: mode ? {
+      19: "icons/icon-19.png",
+      38: "icons/icon-38.png"
+    } : {
+      19: "icons/icon-19-gray.png",
+      38: "icons/icon-38-gray.png"
+    }
+  });
+}
+
+function saveAndEmpty() {
+  browser.bookmarks.getTree(function(results) {
+    results[0].children.forEach(function(toolbar) {
+      if (toolbar.id == id) {
+        browser.storage.local.set({
+          backup: toolbar.children
+        }).then(function() {
+          var len = toolbar.children.length;
+          var count = 1;
+          toolbar.children.forEach(function(bookmark) {
+            browser.bookmarks.update(bookmark.id, {
+              title: "",
+              url: bookmark.url
+            }, function(r) {
+              count++;
+              if (count >= len) {
+                browser.browserAction.enable();
+              }
+            });
+          });
+        }, onError)
+      }
+    });
+  });
+}
+
+function restore() {
+  browser.bookmarks.getTree(function(results) {
+    results[0].children.forEach(function(toolbar) {
+      if (toolbar.id == id) {
+        browser.storage.local.get("backup").then(function(data) {
+          var len = toolbar.children.length;
+          var count = 1;
+          toolbar.children.forEach(function(bookmark) {
+            browser.bookmarks.update(bookmark.id, {
+              title: data.backup.find(item => item.id === bookmark.id).title,
+              url: bookmark.url
+            }, function(r) {
+              count++;
+              if (count >= len) {
+                browser.browserAction.enable();
+              }
+            });
+          });
+        }, onError)
+      }
+    });
+  });
+}
+
+function onGot(data) {
+  if (isEmpty(data)) {
+    browser.storage.local.set({
+      mode: true
+    }).then(function() {
+      saveAndEmpty();
+      changeIcon(true)
+    }, onError)
+  } else {
+    var nmode = !data.mode
+    browser.storage.local.set({
+      mode: nmode
+    }).then(function() {
+      if (nmode) {
+        saveAndEmpty();
+      } else {
+        restore();
+      }
+      changeIcon(nmode)
+    }, onError)
+  }
+}
+
+function toggleCompact() {
+  browser.browserAction.disable();
+  browser.storage.local.get("mode").then(onGot, onError);
+}
+
+function setup() {
+  browser.storage.local.get("mode").then(function(data) {
+    if (!isEmpty(data)) {
+      changeIcon(data.mode);
+    }
+  }, onError);
+}
+
+browser.browserAction.onClicked.addListener(toggleCompact);
+browser.runtime.onUpdateAvailable.addListener(setup);
+browser.runtime.onInstalled.addListener(setup);
